b4f3150b5854624407e1af9bcf6670c980584b63
mattdvhope <mattagape@me.com>
2018-06-19 12:39:19 -0400

    Connected welcome page to Contentful

--- src/components/Welcome-caption.js
+++ src/components/Welcome-caption.js
@@ -205,7 +205,8 @@ export default class WelcomeCaption extends Component {
 
         <Grid fluid>
           <TextContainer>
-            The City English Project makes learning English convenient, affordable and fun!  And, we focus on what you need the most - conversational English skills. Our network offers classes, conversation partnerships and events to help you improve your conversational English. We also integrate culture and worldview elements in to our learning model to better equip your for building relationships within the ASEAN community and beyond.  We want to help you succeed in life and in business! <span id="get-to-be-a-member">Click here</span> to learn more about us. Or, <ModalStyler><ModalApplication getApplication="click here" /></ModalStyler> to sign up for our popular "You Can Speak!" English class.
+            {console.log(this.props.caption)}
+            {this.props.caption} <span id="get-to-be-a-member">Click here</span> to learn more about us. Or, <ModalStyler><ModalApplication getApplication="click here" /></ModalStyler> to sign up for our popular "You Can Speak!" English class.
           </TextContainer>
         </Grid>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,91 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Carousel from '../components/Carousel'
 import WelcomeCaption from '../components/Welcome-caption'
 
-const IndexPage = ({data}) => (
-    <WelcomeCaption />
+export default ({data}) => (
+    <div>
+
+      <Carousel
+        carouselImage1={data.carouselImage1}
+        carouselImage2={data.carouselImage2}
+        carouselImage3={data.carouselImage3}
+        carouselImage4={data.carouselImage4}
+      />
+
+      <WelcomeCaption caption={data.allContentfulWelcome.edges[0].node.welcomeContent.welcomeContent}/>
+
+
+    </div>
 )
 
-export default IndexPage 
+export const homePageQuery = graphql`
+  query HomePage {
+    allContentfulWelcome {
+      edges {
+        node {
+          welcomeContent {
+            welcomeContent
+          }
+        }
+      }
+    }
+
+    carouselImage1: imageSharp(id: { regex: "/1Home-Page-Pic/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    carouselImage2: imageSharp(id: { regex: "/2YouCanSpeakAd/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    carouselImage3: imageSharp(id: { regex: "/3FreeClass/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    carouselImage4: imageSharp(id: { regex: "/4AtYourOffice/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    featureslImage1: imageSharp(id: { regex: "/City-Talk-Class/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    featureslImage2: imageSharp(id: { regex: "/English-Conversation-Partners/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    featureslImage3: imageSharp(id: { regex: "/Conversation-Groups/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    memberlImage1: imageSharp(id: { regex: "/Membership4Web/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    memberlImage2: imageSharp(id: { regex: "/Holistic-Model/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    memberlImage3: imageSharp(id: { regex: "/Friends-helping-friends/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    footerlImage: imageSharp(id: { regex: "/CEP-logo/" }) {
+      sizes(maxWidth: 1240 ) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`
 
