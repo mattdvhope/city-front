1f840c6d063759cf7c4d229bd5702e17b420a4ca
mattdvhope <mattagape@me.com>
2018-06-19 09:50:11 -0400

    Edited html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import WelcomeCaption from '../components/Welcome-caption'
 
-const BlogPost = ({node}) => {
-console.log(node.publishDate);
-
-  return (
-    <li key={node.id}>
-      <Link to={node.slug}>{node.title}</Link>
-      <div>{node.content.childMarkdownRemark.excerpt}</div>
-      <img src={node.featuredImage.resolutions.src} alt=""/>
-    </li>
-  )
-}
 const IndexPage = ({data}) => (
-    <ul>
-      {data.allContentfulBlog.edges.map((edge) => <BlogPost node={edge.node} />)}
-    </ul>
+    <WelcomeCaption />
 )
 
 export default IndexPage 
 
-export const pageQuery = graphql`
-  query pageQuery {
-    allContentfulBlog (
-      filter: {
-        node_locale: {eq: "en-US"}
-      },
-      sort: {
-        fields: [publishDate], order: DESC
-      }
-    ) {
-      edges {
-        node {
-          id
-          title
-          slug
-          publishDate
-          content {
-            childMarkdownRemark {
-              excerpt(pruneLength: 110)
-            }
-          }
-          featuredImage {
-            resolutions(width: 200, height: 100) {
-              src
-              srcSet
-            }
-          }
-        }
-      }
-    }
-  }
-`
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+const BlogPost = ({node}) => {
+  return (
+    <li key={node.id}>
+      {console.log(node.slug)}
+      <Link to={`/${node.slug}`} >{node.title}</Link>
+      <div>{node.content.childMarkdownRemark.excerpt}</div>
+      <img src={node.featuredImage.resolutions.src} alt=""/>
+    </li>
+  )
+}
+const IndexPage = ({data}) => (
+    <ul>
+      {data.allContentfulBlog.edges.map((edge) => <BlogPost node={edge.node} />)}
+    </ul>
 )
 
-export default SecondPage
+export default IndexPage 
+
+export const pageQuery = graphql`
+  query pageQuery {
+    allContentfulBlog (
+      filter: {
+        node_locale: {eq: "en-US"}
+      },
+      sort: {
+        fields: [publishDate], order: DESC
+      }
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          publishDate
+          content {
+            childMarkdownRemark {
+              excerpt(pruneLength: 110)
+            }
+          }
+          featuredImage {
+            resolutions(width: 200, height: 100) {
+              src
+              srcSet
+            }
+          }
+        }
+      }
+    }
+  }
+`
