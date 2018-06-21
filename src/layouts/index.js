e82d262909b99a00d207143054375a96b56031be
mattdvhope <mattagape@me.com>
2018-06-21 18:44:30 -0400

    Added condition and componentWillMount to allow window to be available

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,44 +10,57 @@ import './index.css'
 export default class Layout extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      window: undefined
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ window: window });
   }
 
   handleChangeToThai(event) {
-    event.preventDefault()
+    console.log("in handleChangeToThai");
+    event.preventDefault();
     window.localStorage.setItem("language", "thai" );
     this.setState(this.state);
   }
 
   handleChangeToEnglish(event) {
-    event.preventDefault()
+    console.log("in handleChangeToEnglish");
+    event.preventDefault();
     window.localStorage.setItem("language", "english" );
     this.setState(this.state);
   }
 
   render() {
-    const { data } = this.props;
+    if (this.state.window) {
+      const { data } = this.props;
 
-    return (
-      <div>
-        <Helmet title={data.site.siteMetadata.title} >
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
-          <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/278cd7f5-226e-4ad9-83fb-59e4a7eb4131.css" />
-          <link href="https://fonts.googleapis.com/css?family=Athiti|Chonburi|Kanit|Maitree|Prompt|Sriracha|Taviraj|Trirong|Josefin+Sans" rel="stylesheet" />
-        </Helmet>
+      return (
+        <div>
+          <Helmet title={data.site.siteMetadata.title} >
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
+            <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/278cd7f5-226e-4ad9-83fb-59e4a7eb4131.css" />
+            <link href="https://fonts.googleapis.com/css?family=Athiti|Chonburi|Kanit|Maitree|Prompt|Sriracha|Taviraj|Trirong|Josefin+Sans" rel="stylesheet" />
+          </Helmet>
 
-        <Navbar
-          handleChangeToThai={e => this.handleChangeToThai(e)}
-          handleChangeToEnglish={e => this.handleChangeToEnglish(e)}
-        />
+          <Navbar
+            handleChangeToThai={e => this.handleChangeToThai(e)}
+            handleChangeToEnglish={e => this.handleChangeToEnglish(e)}
+          />
 
-        {this.props.children()}
-      </div>
-    )
+          {this.props.children()}
+        </div>
+      )
+    } else {
+      return <span />
+    }
   }
 }
 
-//// Fix this soon!!  Technical debt!!
+//// Fix this soon!!  Technical debt...
 Layout.propTypes = {
   children: PropTypes.func,
 }
