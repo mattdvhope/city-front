98f22258f7e54f5a17d0c0aec4d9ae78e9cc919f
mattdvhope <mattagape@me.com>
2018-06-27 13:07:08 -0400

    Added Thai language

--- src/pages/more_info.js
+++ src/pages/more_info.js
@@ -1,31 +1,81 @@
-import React from "react";
+import React, { Component } from 'react';
 import Member from '../components/Member'
 
-export default (props) => {
+export default class FrontPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      window: undefined
+    }
+  }
 
-  const { data } = props;
+  componentDidMount() {
+    this.setState({ window: window });
+  }
 
-  return (
-    <Member
-      title={data.allContentfulMember.edges[0].node.title}
-      subtitle={data.allContentfulMember.edges[0].node.subtitle.subtitle}
+  render() {
+    if (this.state.window) {
+      const { data } = this.props;
 
-      memberlImage1={data.memberlImage1}
-      firstListItem={data.allContentfulMember.edges[0].node.firstListItem}
-      secondListItem={data.allContentfulMember.edges[0].node.secondListItem}
-      thirdListItem={data.allContentfulMember.edges[0].node.thirdListItem}
-      fourthListItem={data.allContentfulMember.edges[0].node.fourthListItem}
+      let num1 = []; let num2 = []; let welcomeContent, featuresTitle1, featuresContent1, featuresTitle2, featuresContent2, featuresTitle3, featuresContent3, title, subtitle, firstListItem, secondListItem, thirdListItem, fourthListItem, firstParagraphSubtitle, firstParagraph, secondParagraphSubtitle, secondParagraph;
 
-      memberlImage2={data.memberlImage2}
-      firstParagraphSubtitle={data.allContentfulMember.edges[0].node.firstParagraphSubtitle}
-      firstParagraph={data.allContentfulMemberFirstParagraphTextNode.edges[0].node.firstParagraph}
+      if (process.env.GATSBY_API_URL === "http://localhost:3000") {
+        num1.unshift(1); num2.unshift(0);
+        } else {
+        num1.unshift(0); num2.unshift(1);
+      }
 
-      memberlImage3={data.memberlImage3}
-      secondParagraphSubtitle={data.allContentfulMember.edges[0].node.secondParagraphSubtitle}
-      secondParagraph={data.allContentfulMemberSecondParagraphTextNode.edges[0].node.secondParagraph}
-    />
-  );
+      if (this.state.window.localStorage.language === "thai") {
+        title = data.allContentfulMember.edges[num1[0]].node.title
+        subtitle = data.allContentfulMember.edges[num1[0]].node.subtitle.subtitle
+        firstListItem = data.allContentfulMember.edges[num1[0]].node.firstListItem
+        secondListItem = data.allContentfulMember.edges[num1[0]].node.secondListItem
+        thirdListItem = data.allContentfulMember.edges[num1[0]].node.thirdListItem
+        fourthListItem = data.allContentfulMember.edges[num1[0]].node.fourthListItem
+        firstParagraphSubtitle = data.allContentfulMember.edges[num1[0]].node.firstParagraphSubtitle
+        firstParagraph=data.allContentfulMemberFirstParagraphTextNode.edges[num1[0]].node.firstParagraph
+        secondParagraphSubtitle=data.allContentfulMember.edges[num1[0]].node.secondParagraphSubtitle
+        secondParagraph=data.allContentfulMemberSecondParagraphTextNode.edges[num1[0]].node.secondParagraph
+      } else {
+        title = data.allContentfulMember.edges[num2[0]].node.title
+        subtitle = data.allContentfulMember.edges[num2[0]].node.subtitle.subtitle
+        firstListItem = data.allContentfulMember.edges[num2[0]].node.firstListItem
+        secondListItem = data.allContentfulMember.edges[num2[0]].node.secondListItem
+        thirdListItem = data.allContentfulMember.edges[num2[0]].node.thirdListItem
+        fourthListItem = data.allContentfulMember.edges[num2[0]].node.fourthListItem
+        firstParagraphSubtitle = data.allContentfulMember.edges[num2[0]].node.firstParagraphSubtitle
+        firstParagraph=data.allContentfulMemberFirstParagraphTextNode.edges[num2[0]].node.firstParagraph
+        secondParagraphSubtitle=data.allContentfulMember.edges[num2[0]].node.secondParagraphSubtitle
+        secondParagraph=data.allContentfulMemberSecondParagraphTextNode.edges[num2[0]].node.secondParagraph
+      }
 
+
+
+    return (
+      <Member
+        memberlImage1={data.memberlImage1}
+        memberlImage2={data.memberlImage2}
+        memberlImage3={data.memberlImage3}
+        
+        title={title}
+        subtitle={subtitle}
+
+        firstListItem={firstListItem}
+        secondListItem={secondListItem}
+        thirdListItem={thirdListItem}
+        fourthListItem={fourthListItem}
+
+        firstParagraphSubtitle={firstParagraphSubtitle}
+        firstParagraph={firstParagraph}
+
+        secondParagraphSubtitle={secondParagraphSubtitle}
+        secondParagraph={secondParagraph}
+      />
+    );
+    } else {
+      return <span />
+    }
+  }
 }
 
 export const homePageQuery = graphql`
