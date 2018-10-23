5c9d4291883af1ff8e330dcebcbaa69ac1a3815b
mattdvhope <mattagape@me.com>
2018-10-23 21:50:01 +0700

    Added breaks

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,15 +4,18 @@ import Link from 'gatsby-link'
 const BlogPost = ({node}) => {
   return (
     <li key={node.id}>
+
+    { console.log(node.id) }
+
       <Link to={`/${node.slug}`} >{node.title}</Link>
       <div>{node.content.childMarkdownRemark.excerpt}</div>
       <img src={node.featuredImage.resolutions.src} alt=""/>
     </li>
   )
 }
+
 const IndexPage = ({data}) => (
   <div>
-
     <br/>
     <br/>
     <br/>
