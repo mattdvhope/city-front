df2af1058b42f46e3b678cd232b34e2648a6be25
mattdvhope <mattagape@me.com>
2018-10-26 07:43:23 +0700

    Removed Satorn direction images

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -76,19 +76,8 @@ export default class Layout extends Component {
 
         {this.props.children()}
 
-        <Footer
-          footerlImage={data.footerlImage}
-          fromSatorn1={data.fromSatorn1}
-          fromSatorn2={data.fromSatorn2}
-          fromSatorn3={data.fromSatorn3}
-          fromSatorn4={data.fromSatorn4}
-          fromSatorn5={data.fromSatorn5}
-          fromSatorn6={data.fromSatorn6}
-          fromSatorn7={data.fromSatorn7}
-          fromSatorn8={data.fromSatorn8}
-          fromSatorn9={data.fromSatorn9}
-          fromSatorn10={data.fromSatorn10}
-        />
+        <Footer footerlImage={data.footerlImage} />
+
       </FontStyler>
     )
   }
@@ -108,57 +97,6 @@ export const query = graphql`
     }
 
     footerlImage: imageSharp(id: { regex: "/CEP-logo/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-
-    fromSatorn1: imageSharp(id: { regex: "/From-Satorn-1/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn2: imageSharp(id: { regex: "/From-Satorn-2/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn3: imageSharp(id: { regex: "/From-Satorn-3/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn4: imageSharp(id: { regex: "/From-Satorn-4/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn5: imageSharp(id: { regex: "/From-Satorn-5/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn6: imageSharp(id: { regex: "/From-Satorn-6/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn7: imageSharp(id: { regex: "/From-Satorn-7/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn8: imageSharp(id: { regex: "/From-Satorn-8/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn9: imageSharp(id: { regex: "/From-Satorn-9/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-    fromSatorn10: imageSharp(id: { regex: "/From-Satorn-10/" }) {
       sizes(maxWidth: 1240 ) {
         ...GatsbyImageSharpSizes
       }
