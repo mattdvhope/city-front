06cc9fa836f7b7c68214ce00d84f8b71ca4b8ccd
mattdvhope <mattagape@me.com>
2018-10-26 08:06:22 +0700

    Added Thai for directions

--- src/components/ModalDirections.js
+++ src/components/ModalDirections.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
 
-import { Container, Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'mdbreact';
-
-
 const CenterAddress = styled.div`
   color: #8BC34A;
   font-weight: bold;
@@ -18,9 +15,21 @@ export default class ModalDirections extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      window: undefined,
+      mdbreact: undefined
     };
     this.renderAddress = this.renderAddress.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ window: window });
+    try {
+      const mdbreact = require("mdbreact");
+      this.setState({ mdbreact: mdbreact });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   toggle = () => {
@@ -42,80 +51,105 @@ export default class ModalDirections extends React.Component {
   }
 
   render() {
-    return (
-      <Container>
+    if (this.state.window) {
+      const { data } = this.props;
+      const { Container, Button, Modal, ModalBody, ModalHeader, ModalFooter } = this.state.mdbreact;
 
-        {this.renderAddress(
-          "Address",
-          "(click to see directions)",
-          "66 Pan Road",
-          "Silom, Bangrak",
-          "Bangkok 10500"
-        )}
+      let addressItems;
+      const language = this.state.window.localStorage.language;
+      if (language === "thai") {
+        addressItems = ["ที่อยู",
+                        "(คลิกเพื่อดูเส้นทาง)",
+                        "66 ถนน ปั้น",
+                        "แขวงสีลม เขตบางรัก",
+                        "กรุงเทพมหานคร 10500",
+                        "เส้นทางจากถนนสาทรเหนือเข้าสู่ถนนปั้น"];
+      } else {
+        addressItems = ["Address:",
+                        "(click to see directions)",
+                        "66 Pan Road",
+                        "Silom, Bangrak",
+                        "Bangkok 10500",
+                        "Directions from north Satorn Road entrance to Pan Road"];
+      }
 
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            <div>
+      return (
+        <Container>
 
-              1
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+2.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+          {this.renderAddress(
+            addressItems[0],
+            addressItems[1],
+            addressItems[2],
+            addressItems[3],
+            addressItems[4],
+          )}
 
-              2
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+1.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+          <Modal isOpen={this.state.modal} toggle={this.toggle}>
+            <ModalHeader toggle={this.toggle}>{addressItems[5]}</ModalHeader>
+            <ModalBody>
+              <div>
 
-              3
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+3.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                1
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+2.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              4
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+4.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                2
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+1.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              5
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+5.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                3
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+3.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              6
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+6.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                4
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+4.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              7
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+7.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                5
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+5.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              8
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+8.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                6
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+6.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              9
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP1.png" className="img-fluid" alt="CEP's front entrance" />
-              <hr />
+                7
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+7.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              10
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Front+entrance+driveway+to+CEP.png" className="img-fluid" alt="CEP's front entrance" />
-              <hr />
+                8
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+8.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-              11
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+10.jpg" className="img-fluid" alt="CEP directions" />
-              <hr />
+                9
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP1.png" className="img-fluid" alt="CEP's front entrance" />
+                <hr />
 
-              12
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP2.png" className="img-fluid" alt="CEP's front entrance" />
-              <hr />
+                10
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Front+entrance+driveway+to+CEP.png" className="img-fluid" alt="CEP's front entrance" />
+                <hr />
 
-              13
-              <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP3.png" className="img-fluid" alt="CEP's front entrance" />
+                11
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Directions+from+Satorn/From+Satorn+10.jpg" className="img-fluid" alt="CEP directions" />
+                <hr />
 
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="light-green" onClick={this.toggle}>Close</Button>{' '}
-          </ModalFooter>
-        </Modal>
-      </Container>
-    );
+                12
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP2.png" className="img-fluid" alt="CEP's front entrance" />
+                <hr />
+
+                13
+                <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Pictues+-+CEP3.png" className="img-fluid" alt="CEP's front entrance" />
+
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="light-green" onClick={this.toggle}>Close</Button>{' '}
+            </ModalFooter>
+          </Modal>
+        </Container>
+      );
+    } else {
+      return <span />
+    }
   }
 }
