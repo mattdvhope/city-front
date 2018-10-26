65bd3822a93a0ba3236a3da9b14ba6ff562cb75d
mattdvhope <mattagape@me.com>
2018-10-26 07:45:14 +0700

    Removed Sator directions from graphql

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -53,11 +53,6 @@ export default class Layout extends Component {
         <Helmet title={data.site.siteMetadata.title} >
 
           {/* 
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-         */}
-
-
-          {/* 
           <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/278cd7f5-226e-4ad9-83fb-59e4a7eb4131.css"/>
          */}
 
