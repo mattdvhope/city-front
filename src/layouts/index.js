d096576926af955d8c6e29ab995d42e02523acfe
mattdvhope <mattagape@me.com>
2018-11-08 17:02:34 +0700

    Edit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -57,8 +57,9 @@ export default class Layout extends Component {
          */}
 
           <script src="https://mattagape.wistia.com/medias/98jbnzwejf" async></script>
+
           <script src="https://mattagape.wistia.com/medias/ty23os5uue" async></script>
-          
+
           <script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
 
           <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/50070ebd-d81b-4d29-acc0-f8abd9040636.css"/>
