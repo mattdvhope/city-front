efbe4b0d12f0fd854940a1671222b4a066f07d32
mattdvhope <mattagape@me.com>
2018-12-04 17:09:38 +0700

    Set up social media icons

--- src/pages/social-media.js
+++ src/pages/social-media.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'gatsby-link';
 import styled from "styled-components";
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
@@ -46,11 +47,13 @@ export default class SocialMedia extends React.Component {
       if (language === "thai") {
         textItems = ['ขอขอบคุณที่ลงทะเบียนกับโครงการ City English Project!',
                      'หากคุณต้องการติดต่อเราโปรดใช้ข้อมูลด้านล่างนี้',
-                     'https://scdn.line-apps.com/n/line_add_friends/btn/th.png'];
+                     'https://scdn.line-apps.com/n/line_add_friends/btn/th.png',
+                     'พัฒนาทักษะการฟังของคุณโดยการชมวีดีโอจากเรา'];
       } else {
         textItems = ['Thank you for registering with the City English Project!',
                      'If you would like to contact us, please use the information below.',
-                     'https://scdn.line-apps.com/n/line_add_friends/btn/en.png'];
+                     'https://scdn.line-apps.com/n/line_add_friends/btn/en.png',
+                     'Improve your listening skills by watching our videos.'];
       }
 
       const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(this.state.window.navigator.userAgent);
@@ -90,6 +93,11 @@ export default class SocialMedia extends React.Component {
           <Row>
             <Col xs="12">
               <p className="text-center"><a href={instagramLink}><img height="60" width="60" border="0" alt="เพิ่มเพื่อน" src="https://duckduckgo.com/i/180c8ad1.png" /></a></p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="12">
+              <p className="text-center"><Button><Link to="english-practice" style={{color: `white`}}>{textItems[3]}</Link></Button></p>
             </Col>
           </Row>
         </Container>
